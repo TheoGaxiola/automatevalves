a8d0d9a57095bba23156f4f69ff49a32de74f7db
Theojary Gaxiola <tgaxiola@contractroom.com>
2019-06-21 06:23:22 -0500

    Adding delays and solve indentations issues

--- call_action.py
+++ call_action.py
@@ -1,17 +1,64 @@
+import serial
+import time
+from messaging.sms import SmsDeliver
+
+
 class CallAction():
     def __init__(self, number):
         self.number = number
 
     def call_to_open(self):
-	return None
+        return None
 
     def call_to_close(self):
-	return None
+        return None
 
     def connect_phone(self):
         self.ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=5)
 
-    def wait_for_sms_response(self):
-	return None
+    def get_new_sms_response(self):
+        print("SENDING HELLO")
+        com="ERROR"
+        count=0
+        while(com!="OK"):
+            com=sendCommand("AT")[0]
+            count+=1
+            if(count>5):
+            print "COULD NOT GET A HELLO, all I got was "+com
+            return
+        print(send_command("AT+CMGF=0")[0])
 
-    	
+        while(True):
+            sms = self.read_sms()
+
+            for s in sms:
+            print ""
+            print "SMS"
+            response = s.text
+            time.sleep(1)
+            if response is not None:
+                    killSMS()
+            return response
+
+    def read_sms(self):
+        print("LOOKING FOR SMS")
+        list = send_command("AT+CMGL=0")
+        ret = []
+        for item in list:
+            #print item
+                if item.startswith("+CMGL:") == False:
+            if item!="OK":
+                ret.append(SmsDeliver(item))
+        return ret
+
+    def send_command(self,com):
+        ser.write(com+"\r\n")
+        time.sleep(2)
+        ret = []
+        while ser.inWaiting() > 0:
+            msg = ser.readline().strip()
+            msg = msg.replace("\r","")
+            msg = msg.replace("\n","")
+            if msg!="":
+            ret.append(msg)
+        return ret
--- monitoring_gpio.py
+++ monitoring_gpio.py
@@ -3,10 +3,10 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-banOpen2=0
-banClose2=0
-banOpen3=0
-banClose3=0
+banOpen2 = 0
+banClose2 = 0
+banOpen3 = 0
+banClose3 = 0
 
 #creating an CallAction instance for each valve
 valve1 = CallAction(3314680990)
@@ -19,23 +19,31 @@ GPIO.setup(valve1_input,GPIO.IN)
 GPIO.setup(valve2_input,GPIO.IN)
 
 while True:
+    sleep(.5)
     if GPIO.input(valve1_input) == 1:
         if banOpen2 == 0:
+            sleep(.5)
             valve1.call_to_open()
             banOpen2 = 1
             banClose2 = 0
+    sleep(.5)
     elif GPIO.input(valve1_input) == 0:
         if banClose2 == 0:
+            sleep(.5)
             valve1.call_to_close()
             banClose2 = 1
             banOpen2 = 0
+    sleep(.5)
     elif GPIO.input(valve2_input) == 1:
         if banOpen3 == 0:
+            sleep(.5)
             valve2.call_to_open()
             banOpen3 = 1
-            banClose3 = 0    
+            banClose3 = 0
+    sleep(.5)
     elif GPIO.input(valve2_input) == 0:
         if banClose3 == 0:
+            sleep(.5)
             valve2.call_to_close()
             banClose3 = 1
             banOpen3 = 0
